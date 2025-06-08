6caf0e9ed2c2edcd5f4bf12f497a06a321124db4
nathansaraujo191 <nathansaraujo191@gmail.com>
2025-06-08 15:07:32 -0300

    feat: creating auth middleware to validate token

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,33 @@
+import { Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { AuthRequest } from "../interfaces/AuthRequest";
+
+function authMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Missing or invalid token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!jwtSecret) {
+    throw new Error("Erro ao realizar o login, tente novamente mais tarde.");
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    if (typeof decoded === "object" && "userId" in decoded) {
+      req.user = { userId: (decoded as any).userId as string };
+      next();
+    } else {
+      return res.status(401).json({ message: "Invalid token payload" });
+    }
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+}
+
+module.exports = authMiddleware;
